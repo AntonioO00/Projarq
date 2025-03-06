5760903b184a2df85f76162cd51f61d410cce1b1
Antônio Oliveira <146999054+AntonioO00@users.noreply.github.com>
2025-03-06 23:02:29 +0000

    Criando projeto

--- projeto1a/App.java
+++ projeto1a/App.java
@@ -1,3 +1,4 @@
+package projeto1a;
 import java.util.Scanner;
 
 public class App {
--- projeto1a/Professor.java
+++ projeto1a/Professor.java
@@ -1,3 +1,5 @@
+package projeto1a;
+
 public class Professor {
     private String nome;
     private int anosExperiencia;
